283855a1faaed32e03af5a43ddeb28ca55d3e51d
jacknpoe <jacknpoe@yahoo.com>
2023-04-24 19:20:15 -0300

    Add files via upload

--- cpf.php
+++ cpf.php
@@ -15,7 +15,38 @@
 		function validaCPF( $cpf = "")
 		{
 			$cpf = preg_replace('/[^0-9]/', '', $cpf);
-			echo $cpf;
+
+			if ( strlen( $cpf) != 11)
+			{
+				return false;
+			}
+			
+			if ($cpf == '00000000000' || 
+				$cpf == '11111111111' || 
+				$cpf == '22222222222' || 
+				$cpf == '33333333333' || 
+				$cpf == '44444444444' || 
+				$cpf == '55555555555' || 
+				$cpf == '66666666666' || 
+				$cpf == '77777777777' || 
+				$cpf == '88888888888' || 
+				$cpf == '99999999999')
+			{
+				return false;
+			}
+			 	
+			for ($tempo = 9; $tempo <= 10; $tempo++)
+			{
+				for ($acumulador = 0, $contador = 0; $contador < $tempo; $contador++)
+				{
+					$acumulador += $cpf[ $contador] * (($tempo + 1) - $contador);
+				}
+				$acumulador = ((10 * $acumulador) % 11) % 10;
+				if ( $cpf[ $contador] != $acumulador)
+				{
+					return false;
+				}
+			}
 			return true;
 		}
 	}
