dc5a05e809a3da4245e823785e5495a60483f3c6
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2023-09-27 10:40:24 -0300

    Add files via upload

--- php_testecpf.php
+++ php_testecpf.php
@@ -7,15 +7,31 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<script type="text/javascript">
-			function formatar_mascara(src, mascara)
-			{
-				var campo = src.value.length;
-				var saida = mascara.substring(0,1);
-				var texto = mascara.substring(campo);
-				if(texto.substring(0,1) != saida)
-				{
-					src.value += texto.substring(0,1);
-				}
+			function formatar_mascara(src, mascara){
+			    let tamanho_src = src.value.length
+			    let tamanho_mascara = mascara.length
+			    let atual_src = 0
+			    let atual_mascara = 0
+			    let acumulador = ""
+			    let valor = ""
+			    let temp = ""
+			    for(let i=0; i < tamanho_src; i++){
+			        temp = src.value.substring(i, i+1)
+			        if((!isNaN(Number(temp))) && temp != "-") valor += temp
+			    }
+			    while(true){
+			        if(tamanho_src == atual_src || tamanho_mascara == atual_mascara){
+			            break
+			        }
+			        if(mascara.substring(atual_mascara, atual_mascara + 1) == "9"){
+			            acumulador += valor.substring(atual_src, atual_src + 1)
+			            atual_src ++
+			        } else{
+			            acumulador += mascara.substring(atual_mascara, atual_mascara + 1)
+			        }
+			        atual_mascara ++
+			    }
+			    src.value = acumulador
 			}
 		</script>
 	</head>
@@ -45,7 +61,7 @@
 		<h1>Valida CPF<br></h1>
 
 		<form action="php_testecpf.php" method="POST" style="border: 0px">
-			<p>CPF: <input type="text" maxlength="14" name="cpf" style="width: 100px" value="<?php echo htmlspecialchars( $cpf, ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML401, "ISO-8859-1"); ?>" onkeypress="formatar_mascara( this, '###.###.###-##')" autofocus></p>
+			<p>CPF: <input type="text" maxlength="14" name="cpf" style="width: 100px" value="<?php echo htmlspecialchars( $cpf, ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML401, "ISO-8859-1"); ?>" oninput="formatar_mascara( this, '999.999.999-99')" onchange="formatar_mascara( this, '999.999.999-99')" onkeypress="formatar_mascara( this, '999.999.999-99')" autofocus></p>
 			<p><input type="submit" name="validar" value="Validar"></p>
 		</form>
 
